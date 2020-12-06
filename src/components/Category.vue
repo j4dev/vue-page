7b37b8319755dd210307c8159f76e54d7619b710
j4dev <jesusguanga98@outlook.com>
2020-12-06 17:47:22 -0500

    Identación.

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container mt-3">
-    <div v-for="(categ,index) in category" :key="index">
-      <h1>Usted esta en la categoria: {{categ.name}}</h1>
+    <div v-for="(categ, index) in category" :key="index">
+      <h1 class="prueba">Usted esta en la categoria: {{ categ.name }}</h1>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import { CategoryI }  from "../models/category.interface";
+import { CategoryI } from "../models/category.interface";
 
 @Component
 export default class Category extends Vue {
@@ -20,12 +20,14 @@ export default class Category extends Vue {
     this.getData();
   }
   async getData() {
-    const response = await fetch("https://jsonfy.com/categories/"+this.$route.params.id);
+    const response = await fetch(
+      "https://jsonfy.com/categories/" + this.$route.params.id
+    );
     const json = await response.json();
     this.category = json;
-    
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
